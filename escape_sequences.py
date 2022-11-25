e474f9c99c2f6b6ade53780a63606e7964287e45
rajachowdary100 <rajachowdary.polepalli99@outlook.com>
2022-11-25 09:23:19 +0530

    added the escape_sequences.py file

---
+++ escape_sequences.py
@@ -0,0 +1,5 @@
+# escape sequences
+weather = "It\'s \"kind of\"sunny"
+# "\"-->whatever comes after this, the python recognizes it as a string
+# should be given begining of quotation(single or double)
+print(weather)
